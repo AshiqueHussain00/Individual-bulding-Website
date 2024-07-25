5b8d1cd11c8506f707e4a6ff5049ad7130b48afa
Mushafiya <kmushafiya003@gmail.com>
2024-07-25 21:32:21 +0530

    Made some changes to the file structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,10 @@
+import React from 'react'
+import { NavbarLinks } from '../data/navbar-links'
 
-export const NavbarLinks = {
-  Shape: {
-    name: "Shape",
-    path: "/home",
-  },
-  about: {
-    name: "About",
-    path: "/about",
-  },
-  
-  services: {
-    name: "Services",
-    path: "/services",
-  },
-  Apartment: {
-    name: "Apartment",
-    path: "/apartment",
-  },
-};
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/data/navbar-links.js
@@ -0,0 +1,21 @@
+
+export const NavbarLinks = {
+    Shape: {
+      name: "Shape",
+      path: "/home",
+    },
+    about: {
+      name: "About",
+      path: "/about",
+    },
+    
+    services: {
+      name: "Services",
+      path: "/services",
+    },
+    Apartment: {
+      name: "Apartment",
+      path: "/apartment",
+    },
+  };
+  
