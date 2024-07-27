e0fa61beb546717fa618382bb5bd8d584dfae478
Ashique <ansariazad7864@gmail.com>
2024-07-27 22:04:14 +0530

    Individual Bulding Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-
-import './App.css'
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import AnimatedComponents from './components/AnimatedText'
-import ExampleComponent from './components/ExampleComponent'
-import BuildingSection from './components/BuildingSection'
-import AnimatedContainer from './components/AnimatedContainer'
+import "./App.css";
+import ExampleComponent from "./components/ExampleComponent";
 
 function App() {
-
   return (
-    <div className='bg-black'>
-<AnimatedContainer/>
-       <Navbar/>
-       
-       {/* <ExampleComponent/> */}
-       {/* <BuildingSection/> */}
-      {/* <BuildingSection/> */}
-     {/* <AnimatedComponents/> */}
+    <div className="bg-black">
+      <ExampleComponent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ExampleComponent.jsx
+++ src/components/ExampleComponent.jsx
@@ -1,25 +1,29 @@
 // import React from "react";
-import '/src/index.css';
+import form '/src/index.css'
+import background_image from "/src/images/background_div.png";
 import bulding2 from "/src/images/building2.jpg";
 import bulding3 from "/src/images/building3.jpg";
 
 function ExampleComponent() {
   return (
-    <div className="pt-16 ">
-  
+    <div>
+
 
       {/* ---------- container ------------------- */}
-      <div className='w-[95%] mx-auto '>
+      <div className='  '>
+        <img 
+        src={background_image} 
+        alt='Background Image'
+        className="z-20 absolute"
+        
+        ></img>
 
-
-        <div className="w-[95%] back-element flex justify-between bg-white/30  py-10 px-12 absolute rounded-custom-80 z-30">
-
-          <div className="back-element1"><div className='back-element3'></div> <div className='back-element2'></div></div>
+        <div className=" back-element flex p-10 justify-between  absolute  z-30">
 
 
           {/* ------------------------- left part ------------------------ */}
 
-          <div className="flex bg-transparent w-[32%] b gap-x-6 items-center">
+          <div className="flex bg-transparent w-[38%] gap-5 items-center pr-20">
 
             <div className='w-[400px] h-[130px] '>
 
@@ -31,7 +35,7 @@ function ExampleComponent() {
             </div>
 
             <div className="..">
-              <p className="text-white">
+              <p className="text-black">
                 This is some content that goes beside the image. You can add text,
                 buttons, or any other elements here.
               </p>
@@ -40,10 +44,10 @@ function ExampleComponent() {
 
 
 
-               {/* ------------------ right part ------------------------- */}
+          {/* ------------------ right part ------------------------- */}
 
 
-          <div className="flex gap-x-6 bg-transparent w-[32%] items-center">
+          <div className="flex gap-5 bg-transparent w-[38%] items-center pr-20 ">
 
             <div className='w-[400px] h-[130px]  '>
 
@@ -56,7 +60,7 @@ function ExampleComponent() {
             </div>
 
             <div className="..">
-              <p className="text-white ">
+              <p className="text-black ">
                 This is some content that goes beside the image. You can add text,
                 buttons, or any other elements here.
               </p>
