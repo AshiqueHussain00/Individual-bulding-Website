e7587db28b5c2bec20646a3d27f6f023972b0584
akki9689 <akashpuri259@gmail.com>
2024-07-27 20:35:34 +0530

    animatedcontainer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
-
-
+import AnimatedComponents from './components/AnimatedText'
+import ExampleComponent from './components/ExampleComponent'
 import BuildingSection from './components/BuildingSection'
+import AnimatedContainer from './components/AnimatedContainer'
 
 function App() {
 
   return (
     <div className='bg-black'>
+<AnimatedContainer/>
        <Navbar/>
-      <BuildingSection/>
-     
+       
+       {/* <ExampleComponent/> */}
+       {/* <BuildingSection/> */}
+      {/* <BuildingSection/> */}
+     {/* <AnimatedComponents/> */}
     </div>
   )
 }
