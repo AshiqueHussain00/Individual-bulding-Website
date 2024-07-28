abd2967ad34f80dc64180141be6e318fd7144179
Mushafiya <kmushafiya003@gmail.com>
2024-07-28 12:38:55 +0530

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import "./App.css";
-<<<<<<< HEAD
 import AnimatedComponent from "./components/AnimatedComponent";
-=======
-
->>>>>>> animation
 
 
 function App() {
   return (
     <div className="bg-black">
-<<<<<<< HEAD
       <AnimatedComponent />
-=======
-  
-      
->>>>>>> animation
     </div>
   );
 }
