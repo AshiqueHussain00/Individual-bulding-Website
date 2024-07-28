f90b2bfb811bdbf28120768e10eb1f1a57b05ade
nalini jadhav <nalini@edulab.in>
2024-07-28 13:38:15 +0530

    add herosection code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
+import HeroSection from "./components/HeroSection";
 
-import BuildingSection from "./components/BuildingSection";
+
 
 
 function App() {
   return (
     <div className="bg-black">
-      <BuildingSection />
+      <HeroSection/>
     </div>
   );
 }
